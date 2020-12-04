f7868746d1c5e7a950681c70e06f325d4ea4601c
Elias N Vasylenko <elias@vasylenko.uk>
2020-12-04 16:34:05 +0000

    minor i3 tweaks, firefox includes extensions

--- nixpkgs/overlays/desktop.nix
+++ nixpkgs/overlays/desktop.nix
@@ -8,7 +8,40 @@ self: super: {
     in self.buildEnv {
       name = "desktop";
       paths = [
-        firefox
+        (wrapFirefox firefox-unwrapped {
+          extraExtensions = [
+            (fetchFirefoxAddon {
+              name = "ublock";
+              url = "https://addons.mozilla.org/firefox/downloads/file/3679754/ublock_origin-1.31.0-an+fx.xpi";
+              sha256 = "1h768ljlh3pi23l27qp961v1hd0nbj2vasgy11bmcrlqp40zgvnr";
+            })
+            (fetchFirefoxAddon {
+              name = "noscript";
+              url = "https://addons.mozilla.org/firefox/downloads/file/3673546/noscript_security_suite-11.1.5-an+fx.xpi";
+              sha256 = "162kxcai5yqxiys4dngz8yqqkdqmp4n86kwas5nbdwq20p4nkjh4";
+            })
+          ];
+
+          extraPolicies = {
+            CaptivePortal = false;
+            DisableFirefoxStudies = true;
+            DisablePocket = true;
+            DisableTelemetry = true;
+            FirefoxHome = {
+              Pocket = false;
+              Snippets = false;
+            };
+            UserMessaging = {
+              ExtensionRecommendations = false;
+              SkipOnboarding = true;
+            };
+          };
+
+          extraPrefs = ''
+            // Show more ssl cert infos
+            lockPref("security.identityblock.show_extended_validation", true);
+          '';
+        })
         mpv
         alacritty (runCommand "profile" {} ''
           mkdir -p $out/etc/profile.d
@@ -22,8 +55,8 @@ self: super: {
         (neovim.override {
           configure = {
             customRC = ''
-	      set nu
-	      set mouse=a
+              set nu
+              set mouse=a
             '';
             packages.myVimPackage = with pkgs.vimPlugins; {
               start = [ editorconfig-vim YouCompleteMe ];
